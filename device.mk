7b71346ee72187c7071ee045f07da0c485e1f6c7
Siddharth Pranjale <sidpranjale127@protonmail.com>
2020-11-11 21:52:23 +0530

    DRG: build FP hal

--- device.mk
+++ device.mk
@@ -194,7 +194,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1
+    android.hardware.biometrics.fingerprint@2.1-service
 
 # FM
 PRODUCT_PACKAGES += \
